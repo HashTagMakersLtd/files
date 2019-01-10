13b58c1283343f6b1519a89cd8d53bfc8712abb1
Joey <jyudel@gmail.com>
2019-01-10 13:52:23 +0200

    Google Auth works!

--- MCSign-In.js
+++ MCSign-In.js
@@ -1,5 +1,5 @@
 //Useful references
-var button = $("#")
+var button = $("#googleSI");
 
 //Functions
 
@@ -8,11 +8,42 @@ function signOut() {
   firebase.auth().signOut();
 }
 
+function initFirebaseAuth() {
+  // Listen to auth state changes.
+  firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+function signInWithGoogle(){//Signs in with Google
+	console.log("clicked");
+	var provider = new firebase.auth.GoogleAuthProvider();
+	firebase.auth().signInWithRedirect(provider);
+	firebase.auth().getRedirectResult().then(function(result) {//This gets the result of the redirected login
+	  if (result.credential) {
+	    // This gives you a Google Access Token. You can use it to access the Google API.
+	    var token = result.credential.accessToken;
+	  }
+	  // The signed-in user info.
+	  var user = result.user;
+	  //TODO: add to database
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  console.log(errorMessage);
+	});
+}
+
 function authStateObserver(user) {
 	
 	if (user) { // User is signed in!
-		alert("Already logged in!")
-	else {
-		alert("Not logged in")
+		alert("Already logged in!");
+	} else {
+		button.on("click", signInWithGoogle)
 	}
+}
  
+initFirebaseAuth();
