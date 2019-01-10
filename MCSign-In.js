8c9cc549f1b339671a13d63f91fa8b02d47d85ee
Joey <jyudel@gmail.com>
2019-01-10 13:04:56 +0200

    Update MCSign-In.js
    Started Auth work

--- MCSign-In.js
+++ MCSign-In.js
@@ -1,1 +1,18 @@
-//Add firebase auth stuff
+//Useful references
+var button = $("#")
+
+//Functions
+
+function signOut() {
+  // Sign out of Firebase.
+  firebase.auth().signOut();
+}
+
+function authStateObserver(user) {
+	
+	if (user) { // User is signed in!
+		alert("Already logged in!")
+	else {
+		alert("Not logged in")
+	}
+ 
