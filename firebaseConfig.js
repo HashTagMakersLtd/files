1424b3f5fe33aa72507f7d5223a8035ec81fc07d
JYudelson1 <JYudelson1@users.noreply.github.com>
2019-01-10 16:15:06 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 var config = {
-    apiKey: "AIzaSyCtryRJ4zdoskJE0fQMzVl6Z0XC5_C1V-M",
+    apiKey: "",
     authDomain: "b7makers.firebaseapp.com",
     databaseURL: "https://b7makers.firebaseio.com",
     projectId: "b7makers",
