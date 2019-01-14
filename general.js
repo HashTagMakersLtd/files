e0403fe9bf90889c8da585c29f997d8cf967efb4
SivanShafrir316 <46558766+SivanShafrir316@users.noreply.github.com>
2019-01-14 14:28:29 +0200

    Add files via upload

--- general.js
+++ general.js
@@ -1,33 +1,37 @@
-
-//CHATS
-function addForum(Name){
+function addForum(Name, From){
 	firebase.firestore().collection("forums").add({
     	name: Name
 	})
 	.then(function(docRef) {
 	    console.log("Document written with ID: ", docRef.id);
-	    postMessage(docRef.collection("genChat"),"Welcome to the chat for the "+Name+" forum!")
-	    //TODO: Add Hebrew
+	    var date = new Date();
+	    docRef.collection("genChat").add({
+		    from: From,
+		    text: "Welcome to the chat for the "+Name+" forum!",
+		    timestamp: firebase.firestore.Timestamp.fromDate(date.getTime())
+		})
+		.then(function(docRef) {
+		    console.log("General chat initialized");
+		})
+		.catch(function(error) {
+		    console.error("Error initializing chat: ", error);
+		});
 	})
 	.catch(function(error) {
 	    console.error("Error adding document: ", error);
 	});
 }
 
-function get(name){
-   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-      return decodeURIComponent(name[1]);
-}
+//sidemenu
+$("#toggleMenu").click(function(){
+	if($("#mainMenu").css('width') < '80%') {
+		$("#mainMenu").animate({width: '80%'});
+	}
+});
+$("#backBtn").click(function(){
+	if($("#mainMenu").css('width') > '0') {
+		$("#mainMenu").animate({width: '0'});
+	}
+});
 
-function writeMessageInChat(chatRef, message,userRef){
-		var date = new Date();
-	    chatRef.add({
-		    from: userRef,
-		    text: message,
-		    timestamp: firebase.firestore.Timestamp.fromDate(date)
-		})
-		.catch(function(error) {
-		    console.error("Error posting message: ", error);
-		});
-}
-
+//addForum("Testing", firebase.firestore().collection("users").doc("jyudel@gmail.com"));
